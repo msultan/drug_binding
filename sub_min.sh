83619eb5688a89232f6b558c4732b46e32dc8957
Mohammad <msultan@icme-gpu1.cm.cluster>
2015-12-08 14:59:15 -0800

    Ligand FF

---
+++ sub_min.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-nam=abl
+#SBATCH -o /home/msultan/research/kinase/drug_binding/2016/drug_binding/log.out
+#SBATCH -p longq
+#SBATCH -N 1
+#SBATCH -t 23:59:00
+
+module load openmpi/gcc/64/1.8.5
+echo $CUDA_VISIBLE_DEVICES
+ipcluster start &
+cd /home/msultan/research/kinase/drug_binding/2016/drug_binding/
+python min_all.py abl 
+
+wait 
+
